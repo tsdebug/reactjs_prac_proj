df0b3d1da48a09eb640224465e02ef7d53360bdc
TSdebug <taworkspace01@gmail.com>
2025-08-27 01:56:09 +0530

    A basic react project using create method

---
+++ 00basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Caffe from './Caffe'
+
+function App() {
+  return (
+    <>
+     <Caffe/>
+     <h1>Welcome to Reactjs</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 00basicreact/src/Caffe.js
@@ -0,0 +1,7 @@
+function Caffe(){
+    return(
+        <h2>Coffee & React - using basic create-react-app</h2>
+    )
+};
+
+export default Caffe;
